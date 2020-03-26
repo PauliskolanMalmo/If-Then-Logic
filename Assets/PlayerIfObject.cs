6e4ecacad6c7927794457fe8246c80e6719059c0
Paul B <paul.belfrage@gmail.com>
2020-03-26 12:13:20 +0100

    Challenge Review Master If-Statements P4
    12. Challenge Review Master If-Statements P4

---
+++ Assets/PlayerIfObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIfObject : MonoBehaviour
+{
+    public GameObject cube;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && score <= 50)
+        {
+            score += 25;
+        }
+        if (score > 50)
+        {
+            cube.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
