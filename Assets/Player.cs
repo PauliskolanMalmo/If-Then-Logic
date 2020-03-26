1c40032f3266512f72914a6f1a927ed84fd7cc78
Paul B <paul.belfrage@gmail.com>
2020-03-26 09:01:21 +0100

    Challenge Review Master If-Statements P1
    Challenge Review Master If-Statements P1

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private int points;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            points += 10;
+            //points = points + 10;
+
+        }
+    }
+}
