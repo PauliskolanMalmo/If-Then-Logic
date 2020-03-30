0687e4556924eedc2c132a20ca991b00bd8635a8
Paul B <paul.belfrage@gmail.com>
2020-03-30 11:42:51 +0200

    Challenge Review Speed Program
    Challenge Review Speed Program

---
+++ Assets/PlayerSpeedProgram.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpeedProgram : MonoBehaviour
+{
+    public int speed = 0;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            speed += 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            speed -= 5;
+        }
+
+        if (speed > 20)
+        {
+            print("Slow down");
+        }
+        else if (speed == 0)
+        {
+            print("Speed up");
+        }
+
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+    }
+}
